b3e296c83b16a2faf21952156cc8386e2c722333
mFaYizp <fayizfaz78@gmail.com>
2023-12-13 00:27:56 +0530

    nav sponsors positioned

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <section className='navbar'>
-      <div className='nav-container'>
-      <div className="nav-left">
-        <img src="/assets/logo.png" alt="logo" className='logo-img'/>
-      </div>
+    <section className="navbar">
+      <div className="nav-container">
+        <div className="nav-left">
+          <img src="/assets/logo.png" alt="logo" className="logo-img" />
+        </div>
         <div className="nav-right">
-          <img src="/assets/mcafee.png" alt="sponsor1" className='sponsors'/>
-          <img src="/assets/norton.png" alt="sponsor2" className='sponsors'/>
+          <img src="/assets/mcafee.png" alt="sponsor1" className="sponsors" />
+          <img src="/assets/norton.png" alt="sponsor2" className="sponsors" />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
