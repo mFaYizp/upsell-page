29dbc153f41c65c04c5a0919fda4348a0ba79e6d
mFaYizp <fayizfaz78@gmail.com>
2023-12-14 17:59:25 +0530

    navbar responsive

--- src/components/announcement/Announcement.jsx
+++ src/components/announcement/Announcement.jsx
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./announcement.css";
-import { FavoriteBorderOutlined, LocalShippingOutlined, PublishedWithChangesOutlined, VerifiedOutlined } from "@mui/icons-material";
+import {
+  FavoriteBorderOutlined,
+  LocalShippingOutlined,
+  PublishedWithChangesOutlined,
+  VerifiedOutlined,
+} from "@mui/icons-material";
+
+const announcementItems = [
+  { icon: <VerifiedOutlined />, text: "30-DAY SATISFACTION GUARANTEE" },
+  {
+    icon: <LocalShippingOutlined />,
+    text: "Free delivery on orders over $40.00",
+  },
+  { icon: <FavoriteBorderOutlined />, text: "50.000+ HAPPY CUSTOMERS" },
+  { icon: <PublishedWithChangesOutlined />, text: "100% Money Back Guarantee" },
+];
 
 const Announcement = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prevIndex) => (prevIndex + 1) % announcementItems.length);
+    }, 3000); // Change the interval duration as needed (e.g., 5000ms = 5 seconds)
+
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <section className="announcement">
       <div className="announcement-container">
-        <div className="announcement-items">
-          <VerifiedOutlined />
-          <span>30-DAY SATISFACTION GUARANTEE</span>
-        </div>
-        <div className="announcement-items">
-          <LocalShippingOutlined />
-          <span>Free delivery on orders over $40.00</span>
-        </div>
-        <div className="announcement-items">
-          <FavoriteBorderOutlined />
-          <span>50.000+ HAPPY CUSTOMERS</span>
-        </div>
-        <div className="announcement-items">
-          <PublishedWithChangesOutlined />
-          <span>100% Money Back Guarantee</span>
-        </div>
+        {announcementItems.map((item, index) => (
+          <div
+            className={`announcement-items ${
+              index === activeIndex ? "active" : ""
+            }`}
+            key={index}
+          >
+            {item.icon}
+            <span>{item.text}</span>
+          </div>
+        ))}
       </div>
     </section>
   );
